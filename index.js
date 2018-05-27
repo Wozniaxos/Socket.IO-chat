e9c932c22fee34b4e538ff989afc77bcf47f365a
Wozniaxos <kacperwozniax@gmail.com>
2018-05-27 20:45:26 +0200

    Added online members and validate nick availability.

--- index.js
+++ index.js
@@ -2,20 +2,37 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
+const onlineMembers = [];
+
+app.get('/', (req, res) =>{
   res.sendFile(__dirname + '/views/index.html');
 });
 
-io.on('connection', function(socket){
-  let connectedClient = socket.handshake.query.nickname
+app.get('/online', (req, res) => {
+  res.send(onlineMembers);
+});
+
+io.on('connection', (socket) => {
+  let connectedClient = socket.handshake.query.nickname;
+  onlineMembers.push(connectedClient);
   io.emit('connected info', connectedClient);
-  socket.on('disconnect', function(){
+  io.emit('update online', onlineMembers);
+
+  socket.on('disconnect', () => {
     io.emit('disconnect info', connectedClient);
+    removeOnlineUser(connectedClient);
+    io.emit('update online', onlineMembers);
   });
-  socket.on('chat message', function(msg){
+
+  socket.on('chat message', (msg) => {
     io.emit('chat message', msg, connectedClient);
   });
 });
+
+const removeOnlineUser = (user) => {
+  let index = onlineMembers.indexOf(user);
+  index > -1 ? onlineMembers.splice(index, 1) : false;
+};
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
